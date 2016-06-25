c8660eca757c6b256abc7c4945152e18be6026e7
baixiao <baixiao@codoon.com>
2016-06-25 16:24:50 +0800

    second

--- optimization.go
+++ optimization.go
@@ -11,37 +11,18 @@ import (
 	"math/rand"
 )
 
-//func ReadLine(fileName string) error {
-//	f, err := os.Open(fileName)
-//	if err != nil {
-//		return err
-//	}
-//	buf := bufio.NewReader(f)
-//	for {
-//		line, err := buf.ReadString('\n')
-//		line = strings.TrimSpace(line)
-//		fmt.Println(line)
-//		if err != nil {
-//			if err == io.EOF {
-//				return nil
-//			}
-//			return err
-//		}
-//	}
-//	return nil
-//}
 var GDestination string = "LGA"
 
-var GPeople []interface{} = []interface{}{
-	[2]interface{}{"Seymour","BOS"},
-	[2]interface{}{"Franny","DAL"},
-	[2]interface{}{"Zooey","CAK"},
-	[2]interface{}{"Walt","MIA"},
-	[2]interface{}{"Buddy","ORD"},
-	[2]interface{}{"Les","OMA"},
+var GPeople [][2]string = [][2]string{
+	[2]string{"Seymour","BOS"},
+	[2]string{"Franny","DAL"},
+	[2]string{"Zooey","CAK"},
+	[2]string{"Walt","MIA"},
+	[2]string{"Buddy","ORD"},
+	[2]string{"Les","OMA"},
 }
 
-var GFlight map[[2]string][]interface{}
+var GFlight map[[2]string][][3]string//interface{}里面是[3]string
 
 
 func getMinutes(t string) int64 {
@@ -54,7 +35,7 @@ func getMinutes(t string) int64 {
 }
 
 // 从txt文件中解析航班信息
-func parseShecdule(fileName string, flight *map[[2]string][]interface{}) error {
+func parseShecdule(fileName string, flight *map[[2]string][][3]string) error {
 	f, err := os.Open(fileName)
 	if err != nil {
 		return err
@@ -75,9 +56,9 @@ func parseShecdule(fileName string, flight *map[[2]string][]interface{}) error {
 		origin, dest := lineList[0], lineList[1]
 		depart, arrive, price := lineList[2], lineList[3], lineList[4]
 		if v, ok := (*flight)[[2]string{origin, dest}]; ok {
-			(*flight)[[2]string{origin, dest}] = append(v, [3]interface{}{depart, arrive, price})
+			(*flight)[[2]string{origin, dest}] = append(v, [3]string{depart, arrive, price})
 		} else {
-			(*flight)[[2]string{origin, dest}] = []interface{}{[3]interface{}{depart, arrive, price}}
+			(*flight)[[2]string{origin, dest}] = [][3]string{[3]string{depart, arrive, price}}
 		}
 	}
 	return nil
@@ -86,11 +67,11 @@ func parseShecdule(fileName string, flight *map[[2]string][]interface{}) error {
 //打印选定日程表的航班信息
 func printSchedule(schedule []int)  {
 	for i := 0; i < len(schedule)/2; i++ {
-		name := GPeople[i].([2]interface{})[0].(string)
-		origin := GPeople[i].([2]interface{})[1].(string)
+		name := GPeople[i][0]
+		origin := GPeople[i][1]
 
-		out := GFlight[[2]string{origin, GDestination}][schedule[i]].([3]interface{})
-		ret := GFlight[[2]string{GDestination, origin}][schedule[i+1]].([3]interface{})
+		out := GFlight[[2]string{origin, GDestination}][schedule[i]]
+		ret := GFlight[[2]string{GDestination, origin}][schedule[i+1]]
 
 		fmt.Printf("%10s%10s %5s-%5s $%3s %5s-%5s $%3s\n", name, origin,
 			out[0], out[1], out[2], ret[0], ret[1], ret[2])
@@ -103,36 +84,36 @@ func costSchedule(schedule []int) int64 {
 	eariestDepart = 24*60
 
 	for i := 0; i < len(schedule)/2; i++ {
-		origin := GPeople[i].([2]interface{})[1].(string)
-		out := GFlight[[2]string{origin, GDestination}][schedule[i]].([3]interface{})
-		ret := GFlight[[2]string{GDestination, origin}][schedule[i+1]].([3]interface{})
+		origin := GPeople[i][1]
+		out := GFlight[[2]string{origin, GDestination}][schedule[i]]
+		ret := GFlight[[2]string{GDestination, origin}][schedule[i+1]]
 
-		if price, err := strconv.ParseInt(out[2].(string), 10, 64); err != nil {
+		if price, err := strconv.ParseInt(out[2], 10, 64); err != nil {
 			fmt.Printf("strconv.ParseInt error: %v\n", err)
 		} else {
 			totalPrice += price
 		}
-		if price, err := strconv.ParseInt(ret[2].(string), 10, 64); err != nil {
+		if price, err := strconv.ParseInt(ret[2], 10, 64); err != nil {
 			fmt.Printf("strconv.ParseInt error: %v\n", err)
 		} else {
 			totalPrice += price
 		}
 		//Track the latest arrival and earliest departure
-		if latestArrive < getMinutes(out[1].(string)) {
-			latestArrive = getMinutes(out[1].(string))
+		if latestArrive < getMinutes(out[1]) {
+			latestArrive = getMinutes(out[1])
 		}
-		if eariestDepart > getMinutes(ret[0].(string)) {
-			eariestDepart = getMinutes(ret[0].(string))
+		if eariestDepart > getMinutes(ret[0]) {
+			eariestDepart = getMinutes(ret[0])
 		}
 	}
 
 	for i := 0; i < len(schedule)/2; i++ {
-		origin := GPeople[i].([2]interface{})[1].(string)
-		out := GFlight[[2]string{origin, GDestination}][schedule[i]].([3]interface{})
-		ret := GFlight[[2]string{GDestination, origin}][schedule[i+1]].([3]interface{})
+		origin := GPeople[i][1]
+		out := GFlight[[2]string{origin, GDestination}][schedule[i]]
+		ret := GFlight[[2]string{GDestination, origin}][schedule[i+1]]
 
-		totalWait += (latestArrive - getMinutes(out[1].(string)))
-		totalWait += (getMinutes(ret[1].(string)) - eariestDepart)
+		totalWait += (latestArrive - getMinutes(out[1]))
+		totalWait += (getMinutes(ret[1]) - eariestDepart)
 	}
 
 	//租车超过一天的额外费用
@@ -170,9 +151,8 @@ func optimizeRandom(domainList [][2]int, costF func([]int) int64) []int {
 
 func main() {
 
-	GFlight = make(map[[2]string][]interface{})
+	GFlight = make(map[[2]string][][3]string)
 	err := parseShecdule("/Users/baixiao/Go/src/test/optimization/schedule.txt", &GFlight)
-	//err := ReadLine("/Users/baixiao/Go/src/test/optimization/schedule.txt")
 	if err != nil {
 		fmt.Printf("parseShecdule error: %v\n", err)
 	}
@@ -182,7 +162,7 @@ func main() {
 	}
 
 	//打印看看
-	var schedule []int = []int{1, 4 , 3 , 2 , 7 , 3 , 6, 3 , 2 , 4 , 5, 3}
+	var schedule []int = []int{1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3}//依次表示每个人选择的往返航班的编号
 	fmt.Println("origin schedule", schedule)
 	printSchedule(schedule)
 	//println(getMinutes("8:25"))
