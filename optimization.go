e363c982068dbc1b150cb35860c5106e8721dc17
baixiao <baixiao@codoon.com>
2016-08-29 20:40:56 +0800

    particle swarm optimization

--- optimization.go
+++ optimization.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"math/rand"
 	"math"
+	"sort"
 )
 
 var GDestination string = "LGA"
@@ -246,6 +247,212 @@ func optimizeAnealing(domainList [][2]int, costF func([]int) int64, t, cool floa
 	return bestSchedule
 }
 
+//遗传算法
+func optimizeGenetic(domainList [][2]int, costF func([]int) int64, mutProb, elite float64, popSize, maxIter, step int) []int {
+	popScheduleList := make([][]int, 0)
+
+	fmt.Println(domainList)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	//Build the initial population
+	for i := 0; i < popSize; i ++ {
+		var bestSchedule []int
+		for _, domain := range domainList {
+			choice := r.Intn(domain[1] - domain[0]) + domain[0]
+			bestSchedule = append(bestSchedule, choice)
+		}
+		popScheduleList = append(popScheduleList, bestSchedule)
+	}
+	fmt.Println("1", popScheduleList)
+
+	//How many winners from each generation?
+	topElite := int(elite*float64(popSize))
+
+	//Main loop
+	for i := 0; i < maxIter; i ++ {
+		fmt.Println("loop", i)
+		geneticScoreList := make(GeneticScoreList, 0)
+		for _, schedule := range popScheduleList {
+			score := costF(schedule)
+			geneticScoreList = append(geneticScoreList, GeneticScore{
+				Score: score,
+				Schedule: schedule,
+			})
+		}
+		sort.Sort(geneticScoreList)
+
+		//Start with the pure winners
+		popScheduleList = make([][]int, 0)
+		for j := 0; j < topElite; j ++ {
+			popScheduleList = append(popScheduleList, geneticScoreList[j].Schedule)
+			fmt.Println("sort", geneticScoreList[j].Score, geneticScoreList[j].Schedule)
+		}
+
+		//Add mutated and bred forms of the winners
+		for len(popScheduleList) < popSize {
+			p := r.Float64()
+			if p < mutProb {
+				c := r.Intn(topElite)
+				schedule := mutate(domainList, geneticScoreList[c].Schedule, r, step)
+				fmt.Println("mut", c, geneticScoreList[c].Schedule)
+				popScheduleList = append(popScheduleList, schedule)
+			} else {
+				c1 := r.Intn(topElite)
+				c2 := r.Intn(topElite)
+				schedule := crossover(domainList, geneticScoreList[c1].Schedule, geneticScoreList[c2].Schedule, r)
+				fmt.Println("cross", c1, c2, geneticScoreList[c1].Schedule, geneticScoreList[c2].Schedule, schedule)
+				popScheduleList = append(popScheduleList, schedule)
+			}
+		}
+
+	}
+
+	return popScheduleList[0]
+}
+
+type GeneticScore struct {
+	Score int64
+	Schedule []int
+}
+type GeneticScoreList []GeneticScore
+
+func (p GeneticScoreList) Len() int           { return len(p) }
+func (p GeneticScoreList) Less(i, j int) bool { return p[i].Score < p[j].Score }
+func (p GeneticScoreList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+//Mutation Operation
+func mutate(domainList [][2]int, schedule []int, r *rand.Rand, step int) []int {
+	choice := r.Intn(len(domainList))
+	if r.Float64() < 0.5 && schedule[choice] - step >= domainList[choice][0] {
+		newSchedule := make([]int, len(schedule))
+		copy(newSchedule, schedule)
+		newSchedule[choice] = schedule[choice] - step
+		return newSchedule
+	} else if schedule[choice] + step <= domainList[choice][1] {
+		newSchedule := make([]int, len(schedule))
+		copy(newSchedule, schedule)
+		newSchedule[choice] = schedule[choice] + step
+		return newSchedule
+	} else {
+		return schedule
+	}
+}
+
+//Crossover Operation
+func crossover(domainList [][2]int, schedule1, schedule2 []int, r *rand.Rand) []int {
+	choice := r.Intn(len(domainList))
+	fmt.Println("cross choice", choice)
+	newSchedule := make([]int, len(schedule1))
+	copy(newSchedule, schedule1)
+	fmt.Println(newSchedule)
+	copy(newSchedule[choice:], schedule2[choice:])
+	fmt.Println(newSchedule)
+	return newSchedule
+}
+
+//parSize 粒子数
+//maxIter 迭代数
+//step    速度的跨度
+func optimizeParticleSwarm(domainList [][2]int, costF func([]int) int64, parSize, maxIter, step int) []int {
+	dim := len(domainList)
+	parScheduleList := make([][]int, 0)//单个位置的列表
+	parVelocityList := make([][]int, 0)//单个速度的列表
+	parCostList := make([]int64, 0)//单个最优值的列表
+	parBestScheduleList := make([][]int, 0)//单个最优解的列表
+	var globalCost int64 = 999999//全局最优值
+	globalSchedule := make([]int, 0) //全局最优解
+	var w float64 = 0.5
+	var c1 float64 = 2.0
+	var c2 float64 = 2.0
+
+	fmt.Println(domainList)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	//Build the initial
+	for i := 0; i < parSize; i ++ {
+		var initSchedule []int
+		for _, domain := range domainList {
+			choice := r.Intn(domain[1] - domain[0] + 1) + domain[0]
+			initSchedule = append(initSchedule, choice)
+		}
+		parScheduleList = append(parScheduleList, initSchedule)
+		parBestScheduleList = append(parBestScheduleList, initSchedule)
+
+		var initVelocity []int
+		for j := 0; j < dim; j ++ {
+			choice := r.Intn(3 * step) - step
+			initVelocity = append(initVelocity, choice)
+		}
+		parVelocityList = append(parVelocityList, initVelocity)
+
+		parCostList = append(parCostList, costF(initSchedule))
+
+		if globalCost > parCostList[i] {
+			globalCost = parCostList[i]
+			//globalSchedule = initSchedule
+			globalSchedule = make([]int, 0)
+			for j := 0; j < dim; j ++ {
+				globalSchedule = append(globalSchedule, initSchedule[j])
+			}
+		}
+	}
+	fmt.Println("parScheduleList", parScheduleList)
+	fmt.Println("parVelocityList", parVelocityList)
+	fmt.Println("globalCost", globalCost)
+	fmt.Println("globalSchedule", globalSchedule)
+
+	//Main loop
+	for x := 0; x < maxIter; x ++ {
+		fmt.Println("loop", x)
+		//update
+		for i := 0; i < parSize; i ++ {
+			fmt.Println("bird", i)
+			for j := 0; j < dim; j ++ {
+				parVelocityList[i][j] = int(w*float64(parVelocityList[i][j]) + c1*r.Float64()*float64(parBestScheduleList[i][j]-parScheduleList[i][j]) + c2*r.Float64()*float64(globalSchedule[j]-parScheduleList[i][j]))
+				//要不要限制速度
+				if parVelocityList[i][j] < -4 {
+					parVelocityList[i][j] = -4
+				}
+				if parVelocityList[i][j] > 4 {
+					parVelocityList[i][j] = 4
+				}
+
+				parScheduleList[i][j] = parScheduleList[i][j] + parVelocityList[i][j]
+				if parScheduleList[i][j] < domainList[j][0] {
+					parScheduleList[i][j] = domainList[j][0]
+				}
+				if parScheduleList[i][j] > domainList[j][1] {
+					parScheduleList[i][j] = domainList[j][1]
+				}
+			}
+			//fmt.Println("parVelocityList", parVelocityList[i])
+			//fmt.Println("parScheduleList", parScheduleList[i])
+
+			//get pBest
+			if cost := costF(parScheduleList[i]); parCostList[i] > cost{
+				parCostList[i] = cost
+				parBestScheduleList[i] = parScheduleList[i]
+				fmt.Println("parCostList", parCostList[i])
+				fmt.Println("parBestScheduleList", parBestScheduleList[i])
+			}
+
+			//get gBest
+			if globalCost > parCostList[i] {
+				globalCost = parCostList[i]
+				//globalSchedule = parBestScheduleList[i]
+				globalSchedule = make([]int, 0)
+				for j := 0; j < dim; j ++ {
+					globalSchedule = append(globalSchedule, parBestScheduleList[i][j])
+				}
+				fmt.Println("globalCost", globalCost)
+				fmt.Println("globalSchedule", globalSchedule, costF(globalSchedule))
+			}
+		}
+	}
+
+	fmt.Println("best", globalCost)
+	fmt.Println("bestSchedule", globalSchedule)
+	return globalSchedule
+}
+
 func main() {
 
 	GFlight = make(map[[2]string][][3]string)
@@ -284,9 +491,23 @@ func main() {
 	//fmt.Println("hillclimb cost", costSchedule(bestSchedule))
 
 	//退火法优化
-	bestSchedule = optimizeAnealing(domainList, costSchedule, 1000, 0.95, 1)
-	fmt.Println("anealing schedule", bestSchedule)
+	//bestSchedule = optimizeAnealing(domainList, costSchedule, 1000, 0.95, 1)
+	//fmt.Println("anealing schedule", bestSchedule)
+	//printSchedule(bestSchedule)
+	//fmt.Println("anealing cost", costSchedule(bestSchedule))
+
+	//遗传算法
+	//bestSchedule = optimizeGenetic(domainList, costSchedule, 0.2, 0.2, 100, 10, 1)
+	//fmt.Println("genetic schedule", bestSchedule)
+	//printSchedule(bestSchedule)
+	//fmt.Println("genetic cost", costSchedule(bestSchedule))
+
+	//粒子群算法
+	bestSchedule = optimizeParticleSwarm(domainList, costSchedule, 10, 100, 1)
+	fmt.Println("particle swarm schedule", bestSchedule)
 	printSchedule(bestSchedule)
-	fmt.Println("anealing cost", costSchedule(bestSchedule))
+	fmt.Println("particle swarm cost", costSchedule(bestSchedule))
+
+
 
 }
